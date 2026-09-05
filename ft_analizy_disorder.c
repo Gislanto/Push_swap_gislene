fa08a22beff0f09bdd2ebb019717aa541d6541d2
Gislanto <gilpoiuyt@gmail.com>
2026-09-05 20:22:58 +0000

    Feat: Comecando a criar o push_swap

---
+++ ft_analizy_disorder.c
@@ -0,0 +1,26 @@
+float  ft_analizy_disorder(int *stack, int size)
+{
+    int      i;
+    int      j;
+    long     disorder;
+    long     erros;
+    int      temp;
+
+    i = 0;
+    temp = 0;
+    disorder = 0;
+    erros = 0;
+    while (stack[i] && i < size)
+    {
+        temp = stack[i];
+        j = 0;
+        while (j < size - i)
+        {
+            if (temp > stack[i + j++])
+                erros++;
+        }
+        i++;
+    }
+    disorder = size * (size - 1) / 2;
+    return ((float)erros / disorder);
+}
